e47791d4f688cc156985aa589089572bf02ee501
Ryan McMinds <ryan.mcminds@oregonstate.edu>
2019-07-29 11:57:23 +0200

    bugfixes

--- model/lcGLM_functions.r
+++ model/lcGLM_functions.r
@@ -964,7 +964,7 @@ runStanModel <- function(noData = F, shuffleData = F, shuffleSamples = F, variat
                          chains          = NChains,
                          seed            = seed,
                          chain_id        = (NChains * (i - 1) + (1:NChains)),
-                         pars            = c('aveStD', 'stDProps', 'aveStDMeta', 'metaScales', 'metaVarProps', 'subfactProps', 'subfactMetaProps', 'microbeNewEdges', 'hostNewEdges'),
+                         pars            = c('stDProps', 'metaScales', 'metaVarProps', 'subfactProps', 'microbeNewEdges', 'hostNewEdges'),
                          include         = TRUE,
                          init = 0,
                          init_r          = init_r,
